da4fbc1e3fcb5e51983e78f98a47e276e4bc0378
SergeyBesck <160472712+SergeyBesck@users.noreply.github.com>
2024-09-22 11:00:20 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,56 +1,64 @@
 <?php
 include 'temp/head.php';
 include "temp/nav.php";
-
 include 'temp/database.php';
-
 ?>
 
-<br><br>
-<div class="container-fluid">
+<div class="container">
 <h1 style="text-align:center">Оставить заявку на грузоперевозку</h1>
 
+<form class="row g-3" action="" method="post">
 
+  <div class="col-md-4">
+    <label for="name" class="form-label">Ваше имя:</label>
+    <input type="text" class="form-control" id="name" required name="name">
+  </div>
 
-        <form action="submit_request.php" method="POST">
-        <div class="form-element"style="text-align:center">
-            <label for="name">Ваше имя:</label><br>
-            <input type="text" id="client_name" name="client_name" required><br><br>
+  <div class="col-md-4">
+    <label for="phone" class="form-label">Телефон:</label>
+    <input type="phone" class="form-control" id="phone" required name="phone">
+  </div>
 
-            <label for="phone">Телефон:</label><br>
-            <input type="tel" id="phone" name="phone" required><br><br>
+  <div class="col-4">
+    <label for="email" class="form-label">Электронная почта:</label>
+    <input type="email" class="form-control" id="email" required name="email">
+  </div>
 
-            <label for="email">Электронная почта:</label><br>
-            <input type="email" id="client_contact" name="client_contact" required><br><br>
+  <div class="col-12">
+    <label for="name_g" class="form-label">Название груза:</label>
+    <input type="text" class="form-control" id="name_g" required name="name_g">
+  </div>
 
-            <label for="name">Название груза:</label><br>
-            <input type="text" id="cargo_name" name="cargo_name" required><br><br>
+  <div class="col-md-6">
+    <label for="otprav" class="form-label">Место отправления:</label>
+    <input type="text" class="form-control" id="otprav" required placeholder="Брянск ул.Ленина 145" name="otprav">
+  </div>
 
-            <label for="cargo">Тип груза:</label><br>
-            
+  <div class="col-md-6">
+    <label for="naznach" class="form-label">Место назначения:</label>
+    <input type="text" class="form-control" id="naznach" required placeholder="Брянск ул.Кирова 14" name="naznach">
+  </div>
 
-            <SELECT name="Tech">
-<OPTION value="Выберите тип груза">Выберите тип груза </OPTION>
-<OPTION value="Общий груз ">Общий груз</OPTION>
-<OPTION value="Жидкости"> Жидкости</OPTION>
-<OPTION value="Хрупкий груз">Хрупкий груз</OPTION>
-<OPTION value="Тяжелый груз ">Тяжелый груз</OPTION>
-<OPTION value="Опасный груз">Опасный груз </OPTION>
-<OPTION value="Охлажденный груз">Охлажденный груз</OPTION>
-</SELECT>
-            <br><br>
+  <div class="col-md-12">
+    <label for="inputState" class="form-label">Тара</label>
+    <select id="inputState" required class="form-select">
+      <option selected>Выберите...</option>
+      <option>Коробка S до 2кг размеры(15 ширина, 25 длина, 10 высота)</option>
+      <option>Коробка M до 5кг размеры(25 ширина, 35 длина, 15 высота)</option>
+      <option>Коробка L до 12кг размеры(30 ширина, 45 длина, 20 высота)</option>
+    </select>
+  </div>
 
-            <label for="pickup">Место отправления:</label><br>
-            <input type="text" id="cargo_departure" name="cargo_departure" required><br><br>
+  <div class="col-md-3">
+    <label for="data" class="form-label">Дата перевозки:</label>
+    <input type="date" class="form-control" required id="data" name="data">
+  </div>
 
-            <label for="destination">Место назначения:</label><br>
-            <input type="text" id="cargo_destination" name="destination" required><br><br>
+  <div class="col-6"><br>
+    <button type="submit" class="btn btn-primary">Отправить заявку</button>
+  </div>
+</form>
 
-            <label for="date">Дата перевозки:</label><br>
-            <input type="date" id="date" name="date" required><br><br>
-
-            <button type="submit">Отправить заявку</button>
-        </form>
     </main>
     <?php
 include 'temp/footer.php';
