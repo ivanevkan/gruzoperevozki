0586e0074b0f282b7aa140fe393987452cb55159
SergeyBesck <160472712+SergeyBesck@users.noreply.github.com>
2024-09-23 22:52:50 +0300

    Add files via upload

--- submit_request.php
+++ submit_request.php
@@ -12,7 +12,7 @@
         $box = explode(".", $_POST["box"]);
         $razmer = $box[0];
         $size = 'x: '.$box[1].' y: '.$box[2].' z: '.$box[3];
-        $sql = "INSERT INTO `request`(`client_name`, `client_contact`, `cargo_name`, `cargo_departure`, `cargo_destination`, `request_path_distance`, `request_delivery_date`, `cargo_weight`, `cargo_dimensions`, `request_status`) VALUES ('$name','$email','$name_g','$otprav','$naznach','$rast','$data','$razmer','$size','ожидает')";
+        $sql = "INSERT INTO `request`(`client_name`, `client_contact`, `cargo_name`, `cargo_departure`, `cargo_destination`, `request_path_distance`, `request_delivery_date`, `cargo_weight`, `cargo_dimensions`, `request_status`) VALUES ('$name','$email','$name_g','$otprav','$naznach','$rast','$data','$razmer','$size','создана')";
         $res = $mysqli->query($sql);
         if($res =! null){
             var_dump($sql);
